c73672b725047ed175de9faf50080dcaa64dc9ed
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-20 01:06:11 +0100

    created file exercises/2806ea2f-50c2-49a1-b2b7-ed6c8ee01bf9/skeletons/348dbdbc-1401-44a6-a14c-ecf6dfda85e1/s.py

---
+++ exercises/2806ea2f-50c2-49a1-b2b7-ed6c8ee01bf9/skeletons/348dbdbc-1401-44a6-a14c-ecf6dfda85e1/s.py
@@ -0,0 +1,29 @@
+class Adres:
+    def __init__(self, miejscowosc, kod_pocztowy, ulica, nr_domu, nr_mieszkania="", kraj = "Polska"):
+        self.kraj = kraj
+        self.miejscowosc = miejscowosc
+        self.kod_pocztowy = kod_pocztowy
+        self.ulica = ulica
+        self.nr_domu = nr_domu
+        self.nr_mieszkania = nr_mieszkania
+                
+    def __str__(self):        
+        adres = self.ulica + " " + self.nr_domu
+        if self.nr_mieszkania: adres += "/" + self.nr_mieszkania
+        adres += ", " + self.kod_pocztowy + " " + self.miejscowosc + ", " + self.kraj
+        return adres
+		
+a=Adres("", "", "Mickiewicza", "")
+a.podaj_adres()
+print(a)
+#Miejscowosc: 
+#Miejscowosc wymaga podania wartości.
+#Miejscowosc: Szczecin
+#Kod Pocztowy: 
+#Kod Pocztowy wymaga podania wartości.
+#Kod Pocztowy: 71-101
+#Ulica (Mickiewicza): 
+#Nr Domu: 64
+#Nr Mieszkania: 
+#Kraj (Polska): 
+#Mickiewicza 64, 71-101 Szczecin, Polska
