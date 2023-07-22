6892981e8550c5d1109746dbad4411853713bab4
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-22 08:46:51 +0100

    updated file exercises/7b87eca1-92ff-4213-9a09-9a64178382ab/solutions/23b67e69-0959-431f-acba-1ba852386c82/sol.py

--- exercises/7b87eca1-92ff-4213-9a09-9a64178382ab/solutions/23b67e69-0959-431f-acba-1ba852386c82/sol.py
+++ exercises/7b87eca1-92ff-4213-9a09-9a64178382ab/solutions/23b67e69-0959-431f-acba-1ba852386c82/sol.py
@@ -19,7 +19,7 @@ class Adres:
             if self.__dict__.get(x, ""): dotychczasowe = " (" + self.__dict__[x] + ")"
             x_sformatowane = x.title().replace("_", " ")
             while True:
-                v = input(x_sformatowane + dotychczasowe + ": ")
+                v = input(x_sformatowane + dotychczasowe + ":\n")
                 if v:
                     self.__dict__[x] = v
                     break
